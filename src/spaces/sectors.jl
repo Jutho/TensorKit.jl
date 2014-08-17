5a62aaf99d7384be060d1a984c3aa0212ba273f7
Jutho Haegeman <jutho.haegeman@ugent.be>
2014-08-17 17:11:20 +0200

    a lot of work on invariant tensors

---
+++ src/spaces/sectors.jl
@@ -0,0 +1,37 @@
+# sectors.jl
+# Explicit realization of sectors to be used for gradation of vector spaces.
+
+# Abelian Sectors:
+#-----------------
+immutable Parity <: Abelian
+    p::Bool
+end
+Parity(d::Integer)=Parity(iseven(d))
+
+*(c1::Parity,c2::Parity)=Parity(c1.p != c2.p)
+Base.conj(c::Parity)=Parity(!c.p)
+Base.one(c::Parity)=Parity(false)
+Base.one(::Type{Parity})=Parity(false)
+
+immutable ZNCharge{N} <: Abelian
+    charge::Int
+    ZNCharge(charge::Int)=new(mod(charge,N))
+end
+
+*{N}(c1::ZNCharge{N},c2::ZNCharge{N})=ZNCharge{N}(c1.charge+c2.charge)
+Base.conj{N}(c::ZNCharge{N})=ZNCharge{N}(-c.charge)
+Base.one{N}(c::ZNCharge{N})=ZNCharge{N}(0)
+Base.one{N}(::Type{ZNCharge{N}})=ZNCharge{N}(0)
+
+immutable U1Charge <: Abelian
+    charge::Int
+end
+
+*(c1::U1Charge,c2::U1Charge)=U1Charge(c1.charge+c2.charge)
+Base.conj(c::U1Charge)=U1Charge(-c.charge)
+Base.one(c::U1Charge)=U1Charge(0)
+Base.one(::Type{U1Charge})=U1Charge(0)
+
+# Nonabelian Sectors:
+#---------------------
+# to be done
