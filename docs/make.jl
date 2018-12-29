7871e550b167e2a35e3bd3bb7bed7b50f51cc5b3
Jutho Haegeman <jutho.haegeman@ugent.be>
2018-12-29 13:54:18 +0100

    enable services

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,12 @@ using Documenter
 using TensorKit
 
 makedocs(modules=[TensorKit],
-            format=:html,
             sitename="TensorKit.jl",
+            authors = "Jutho Haegeman",
             pages = [
                 "Home" => "index.md",
                 "Manual" => ["man/intro.md", "man/spaces.md", "man/sectors.md"],
                 "Library" => ["lib/spaces.md"]
             ])
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-# deploydocs(
-#     deps = nothing,
-#     make = nothing,
-#     target = "build",
-#     repo = "github.com/Jutho/TensorKit.jl.git",
-#     julia = "1.0"
-# )
+deploydocs(repo = "github.com/Jutho/TensorKit.jl.git")
