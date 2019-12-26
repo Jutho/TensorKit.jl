5413a713f80a48e95cef0c9d9b814f104d2e3a01
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-12-26 18:22:43 +0100

    Switch math engine back to MathJaX

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,8 @@ using TensorKit
 makedocs(modules=[TensorKit],
             sitename="TensorKit.jl",
             authors = "Jutho Haegeman",
-            format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+            format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true",
+                                     mathengine = Documenter.MathJax()),
             pages = [
                 "Home" => "index.md",
                 "Manual" => ["man/intro.md", "man/spaces.md", "man/sectors.md", "man/tensors.md"],
