8a9376b9618a61df3d8256875b1df0e6bea7c7ab
Jutho Haegeman <jutho.haegeman@ugent.be>
2020-08-31 00:31:50 +0200

    7) fix and improve deprecation warnings

--- src/auxiliary/deprecate.jl
+++ src/auxiliary/deprecate.jl
@@ -13,27 +13,39 @@ Base.@deprecate(
     permuteind(t::TensorMap, p1::IndexTuple, p2::IndexTuple=(); copy::Bool = false),
     permute(t, p1, p2; copy = copy))
 
+@noinline function Base.getindex(::ComplexNumbers, I::Type{<:Group})
+    S = Rep[I]
+    if repr(S) == "GradedSpace[Irrep[$I]]"
+        Base.depwarn("`ℂ[$I]` is deprecated because $I represents a group rather than its representations, use `ℂ[Irrep[$I]]`, `Rep[$I]` or `GradedSpace[Irrep[$I]]` instead.",
+        ((Base.Core).Typeof(Base.getindex)).name.mt.name)
+    else
+        Base.depwarn("`ℂ[$I]` is deprecated because $I represents a group rather than its representations, use `ℂ[Irrep[$I]]`, `Rep[$I]`, `$S` or `GradedSpace[Irrep[$I]]` instead.",
+        ((Base.Core).Typeof(Base.getindex)).name.mt.name)
+    end
+    return Rep[I]
+end
+
 @noinline function ℤ₂(args...)
     Base.depwarn("`ℤ₂(args...)` is deprecated, use `Z2Irrep(args...)` or ``Irrep[ℤ₂](args...)` instead.", ((Base.Core).Typeof(ℤ₂)).name.mt.name)
-    Irrep{ℤ₂}(args...)
+    Irrep[ℤ₂](args...)
 end
 @noinline function ℤ₃(args...)
     Base.depwarn("`ℤ₃(args...)` is deprecated, use `Z3Irrep(args...)` or ``Irrep[ℤ₃](args...)` instead.", ((Base.Core).Typeof(ℤ₃)).name.mt.name)
-    Irrep{ℤ₃}(args...)
+    Irrep[ℤ₃](args...)
 end
 @noinline function ℤ₄(args...)
     Base.depwarn("`ℤ₄(args...)` is deprecated, use `Z4Irrep(args...)` or ``Irrep[ℤ₄](args...)` instead.", ((Base.Core).Typeof(ℤ₄)).name.mt.name)
-    Irrep{ℤ₄}(args...)
+    Irrep[ℤ₄](args...)
 end
 @noinline function U₁(args...)
     Base.depwarn("`U₁(args...)` is deprecated, use `U1Irrep(args...)` or ``Irrep[U₁](args...)` instead.", ((Base.Core).Typeof(U₁)).name.mt.name)
-    Irrep{U₁}(args...)
+    Irrep[U₁](args...)
 end
 @noinline function CU₁(args...)
     Base.depwarn("`CU₁(args...)` is deprecated, use `CU1Irrep(args...)` or ``Irrep[CU₁](args...)` instead.", ((Base.Core).Typeof(CU₁)).name.mt.name)
-    Irrep{CU₁}(args...)
+    Irrep[CU₁](args...)
 end
 @noinline function SU₂(args...)
     Base.depwarn("`SU₂(args...)` is deprecated, use `SU2Irrep(args...)` or ``Irrep[SU₂](args...)` instead.", ((Base.Core).Typeof(SU₂)).name.mt.name)
-    Irrep{SU₂}(args...)
+    Irrep[SU₂](args...)
 end
