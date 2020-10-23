ca50baaa420dfcf1d46400766f65dd5d0768171b
Jutho Haegeman <jutho.haegeman@ugent.be>
2020-10-23 15:57:36 +0200

    fix deligne

--- src/spaces/deligne.jl
+++ src/spaces/deligne.jl
@@ -18,35 +18,35 @@ natural representation spaces of the direct product of two groups.
 function ⊠(V::GradedSpace, P0::ProductSpace{<:EuclideanSpace{ℂ},0})
     I1 = sectortype(V)
     I2 = sectortype(P0)
-    return GradedSpace[I1 ⊠ I2](ifelse(isdual(V), dual(c), c) ⊠ one(I2) => dim(V, c) for c in sectors(V); dual = isdual(V))
+    return Vect[I1 ⊠ I2](ifelse(isdual(V), dual(c), c) ⊠ one(I2) => dim(V, c) for c in sectors(V); dual = isdual(V))
 end
 
 function ⊠(P0::ProductSpace{<:EuclideanSpace{ℂ},0}, V::GradedSpace)
     I1 = sectortype(P0)
     I2 = sectortype(V)
-    return GradedSpace[I1 ⊠ I2](one(I1) ⊠ ifelse(isdual(V), dual(c), c) => dim(V, c) for c in sectors(V); dual = isdual(V))
+    return Vect[I1 ⊠ I2](one(I1) ⊠ ifelse(isdual(V), dual(c), c) => dim(V, c) for c in sectors(V); dual = isdual(V))
 end
 
 function ⊠(V::ComplexSpace, P0::ProductSpace{<:EuclideanSpace{ℂ},0})
     I2 = sectortype(P0)
-    return GradedSpace[I2](one(I2) => dim(V); dual = isdual(V))
+    return Vect[I2](one(I2) => dim(V); dual = isdual(V))
 end
 
 function ⊠(P0::ProductSpace{<:EuclideanSpace{ℂ},0}, V::ComplexSpace)
     I1 = sectortype(P0)
-    return GradedSpace[I1](one(I1) => dim(V); dual = isdual(V))
+    return Vect[I1](one(I1) => dim(V); dual = isdual(V))
 end
 
 function ⊠(P::ProductSpace{<:EuclideanSpace{ℂ},0}, P0::ProductSpace{<:EuclideanSpace{ℂ},0})
     I1 = sectortype(P)
     I2 = sectortype(P0)
-    return one(GradedSpace[I1 ⊠ I2])
+    return one(Vect[I1 ⊠ I2])
 end
 
 function ⊠(P::ProductSpace{<:EuclideanSpace{ℂ}}, P0::ProductSpace{<:EuclideanSpace{ℂ},0})
     I1 = sectortype(P)
     I2 = sectortype(P0)
-    S = GradedSpace[I1 ⊠ I2]
+    S = Vect[I1 ⊠ I2]
     N = length(P)
     return ProductSpace{S,N}(map(V->V ⊠ P0, tuple(P...)))
 end
@@ -54,7 +54,7 @@ end
 function ⊠(P0::ProductSpace{<:EuclideanSpace{ℂ},0}, P::ProductSpace{<:EuclideanSpace{ℂ}})
     I1 = sectortype(P0)
     I2 = sectortype(P)
-    S = GradedSpace[I1 ⊠ I2]
+    S = Vect[I1 ⊠ I2]
     N = length(P)
     return ProductSpace{S,N}(map(V->P0 ⊠ V, tuple(P...)))
 end
