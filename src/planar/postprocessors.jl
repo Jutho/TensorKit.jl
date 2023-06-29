f5ad35387956c2eca076e2a431817bb9e01b6155
lkdvos <lukas.devos@ugent.be>
2023-06-29 10:00:46 +0200

    Changes to @planar

--- src/planar/postprocessors.jl
+++ src/planar/postprocessors.jl
@@ -4,23 +4,24 @@
 # are not identified by the parsing step of TensorOperations, so we have to manually fix this
 # Step 1: we have to find the new name that TO.tensorify assigned to these temporaries
 # since it parses `tmp[] := a[] * b[]` as `newtmp = similar...; tmp = contract!(.... , newtmp, ...)`
+
+const _PLANAR_OPERATIONS = (:planaradd!, :planarcontract!, :planartrace!)
+const _TENSOR_OPERATIONS = (:tensoradd!, :tensorcontract!, :tensortrace!)
+
+_is_tensoroperation(ex::Expr) = ex.head == :call && (ex.args[1] in _TENSOR_OPERATIONS)
+_is_tensoroperation(ex) = false
+
 function _update_temporaries(ex, temporaries)
     if ex isa Expr && ex.head == :(=)
         lhs = ex.args[1]
         i = findfirst(==(lhs), temporaries)
         if i !== nothing
             rhs = ex.args[2]
-            if rhs isa Expr && rhs.head == :call && rhs.args[1] == :add!
-                newname = rhs.args[6]
-                temporaries[i] = newname
-            elseif rhs isa Expr && rhs.head == :call && rhs.args[1] == :contract!
-                newname = rhs.args[8]
-                temporaries[i] = newname
-            elseif rhs isa Expr && rhs.head == :call && rhs.args[1] == :trace!
-                newname = rhs.args[6]
-                temporaries[i] = newname
+            if _is_tensoroperation(rhs)
+                temporaries[i] = rhs.args[2]
             else
-                @error "lhs = $lhs , rhs = $rhs"
+                temporaries[i] = rhs
+                # error("lhs = $lhs , rhs = $rhs")
             end
         end
     elseif ex isa Expr
@@ -32,50 +33,50 @@ function _update_temporaries(ex, temporaries)
 end
 
 # Step 2: we find `newtmp = similar_from_...` and replace with `newtmp = cached_similar_from...`
+# this is not necessary anymore?
 function _annotate_temporaries(ex, temporaries)
-    if ex isa Expr && ex.head == :(=)
-        lhs = ex.args[1]
-        i = findfirst(==(lhs), temporaries)
-        if i !== nothing
-            rhs = ex.args[2]
-            if !(rhs isa Expr && rhs.head == :call && rhs.args[1] == :similar_from_indices)
-                @error "lhs = $lhs , rhs = $rhs"
-            end
-            newrhs = Expr(:call, :cached_similar_from_indices,
-                            QuoteNode(lhs), rhs.args[2:end]...)
-            return Expr(:(=), lhs, newrhs)
-        end
-    elseif ex isa Expr
-        return Expr(ex.head, [_annotate_temporaries(a, temporaries) for a in ex.args]...)
-    end
+    # if ex isa Expr && ex.head == :(=)
+    #     lhs = ex.args[1]
+    #     i = findfirst(==(lhs), temporaries)
+    #     if i !== nothing
+    #         rhs = ex.args[2]
+    #         if !(rhs isa Expr && rhs.head == :call && rhs.args[1] == :similar_from_indices)
+    #             @error "lhs = $lhs , rhs = $rhs"
+    #         end
+    #         newrhs = Expr(:call, :cached_similar_from_indices,
+    #                         QuoteNode(lhs), rhs.args[2:end]...)
+    #         return Expr(:(=), lhs, newrhs)
+    #     end
+    # elseif ex isa Expr
+    #     return Expr(ex.head, [_annotate_temporaries(a, temporaries) for a in ex.args]...)
+    # end
     return ex
 end
 
 # add correct modules (`GlobalRef`) to various functions
-const _TOFUNCTIONS = (:similar_from_indices, :cached_similar_from_indices,
-                        :scalar, :IndexError)
-
 function _add_modules(ex::Expr)
-    if ex.head == :call && ex.args[1] in _TOFUNCTIONS
-        return Expr(ex.head, GlobalRef(TensorOperations, ex.args[1]),
-                        (_add_modules(ex.args[i]) for i in 2:length(ex.args))...)
-    elseif ex.head == :call && ex.args[1] == :add!
-        @assert ex.args[4] == :(:N)
-        argind = [2,3,5,6,7,8]
-        return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planar_add!)),
-                        (_add_modules(ex.args[i]) for i in argind)...)
-    elseif ex.head == :call && ex.args[1] == :trace!
-        @assert ex.args[4] == :(:N)
-        argind = [2,3,5,6,7,8,9,10]
-        return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planar_trace!)),
-                        (_add_modules(ex.args[i]) for i in argind)...)
-    elseif ex.head == :call && ex.args[1] == :contract!
-        @assert ex.args[4] == :(:N) && ex.args[6] == :(:N)
-        argind = vcat([2,3,5], 7:length(ex.args))
-        return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planar_contract!)),
-                        (_add_modules(ex.args[i]) for i in argind)...)
-    else
-        return Expr(ex.head, (_add_modules(e) for e in ex.args)...)
+    if ex.head == :call
+        if ex.args[1] == :tensoradd!
+            conjA = popat!(ex.args, 5)
+            @assert conjA == :(:N) "conj flag should be `:N` ($conjA)"
+            return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planaradd!)),
+                        map(_add_modules, ex.args[2:end])...)
+        elseif ex.args[1] == :tensorcontract!
+            conjB = popat!(ex.args, 9)
+            conjA = popat!(ex.args, 6)
+            @assert conjA == conjB == :(:N) "conj flag should be `:N` ($conjA), ($conjB)"
+            return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planarcontract!)),
+                        map(_add_modules, ex.args[2:end])...)
+        elseif ex.args[1] == :tensortrace!
+            conjA = popat!(ex.args, 6)
+            @assert conjA == :(:N) "conj flag should be `:N` ($conjA)"
+            return Expr(ex.head, GlobalRef(TensorKit, Symbol(:planartrace!)),
+                        map(_add_modules, ex.args[2:end])...)
+        elseif ex.args[1] in TensorOperations.tensoroperationsfunctions
+            return Expr(ex.head, GlobalRef(TensorOperations, ex.args[1]),
+                        map(_add_modules, ex.args[2:end])...)
+        end
     end
+    return Expr(ex.head, (_add_modules(e) for e in ex.args)...)
 end
 _add_modules(ex) = ex
