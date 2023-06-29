5b1f085ef232778ed4f05d8eed209943f0c840bc
lkdvos <lukas.devos@ugent.be>
2023-06-29 10:00:46 +0200

    Changes to @planar

--- src/planar/planaroperations.jl
+++ src/planar/planaroperations.jl
@@ -1,78 +1,141 @@
 # planar versions of tensor operations add!, trace! and contract!
-planar_add!(α, tsrc::AbstractTensorMap{S},
-            β, tdst::AbstractTensorMap{S, N₁, N₂},
-            p1::IndexTuple{N₁}, p2::IndexTuple{N₂}) where {S, N₁, N₂} =
-    add_transpose!(α, tsrc, β, tdst, p1, p2)
-
-function planar_trace!(α, tsrc::AbstractTensorMap{S},
-                       β, tdst::AbstractTensorMap{S, N₁, N₂},
-                       p1::IndexTuple{N₁}, p2::IndexTuple{N₂},
-                       q1::IndexTuple{N₃}, q2::IndexTuple{N₃}) where {S, N₁, N₂, N₃}
-    if BraidingStyle(sectortype(S)) == Bosonic()
-        return trace!(α, tsrc, β, tdst, p1, p2, q1, q2)
-    end
-
-    @boundscheck begin
-        all(i->space(tsrc, p1[i]) == space(tdst, i), 1:N₁) ||
-            throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
-                    tdst = $(codomain(tdst))←$(domain(tdst)), p1 = $(p1), p2 = $(p2)"))
-        all(i->space(tsrc, p2[i]) == space(tdst, N₁+i), 1:N₂) ||
-            throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
-                    tdst = $(codomain(tdst))←$(domain(tdst)), p1 = $(p1), p2 = $(p2)"))
-        all(i->space(tsrc, q1[i]) == dual(space(tsrc, q2[i])), 1:N₃) ||
-            throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
-                    q1 = $(q1), q2 = $(q2)"))
-    end
-
-    if iszero(β)
-        fill!(tdst, β)
-    elseif β != 1
-        rmul!(tdst, β)
-    end
-    pdata = (p1..., p2...)
-    for (f1, f2) in fusiontrees(tsrc)
-        for ((f1′, f2′), coeff) in planar_trace(f1, f2, p1, p2, q1, q2)
-            TO._trace!(α*coeff, tsrc[f1, f2], true, tdst[f1′, f2′], pdata, q1, q2)
-        end
-    end
-    return tdst
+function planaradd!(C::AbstractTensorMap{S,N₁,N₂}, A::AbstractTensorMap{S},
+                    pA::Index2Tuple{N₁,N₂}, α, β) where {S,N₁,N₂}
+    return add_transpose!(α, A, β, C, pA...)
 end
 
-function planar_contract!(α, A::AbstractTensorMap{S}, B::AbstractTensorMap{S},
-                            β, C::AbstractTensorMap{S},
-                            oindA::IndexTuple, cindA::IndexTuple,
-                            oindB::IndexTuple, cindB::IndexTuple,
-                            p1::IndexTuple, p2::IndexTuple,
-                            syms::Union{Nothing, NTuple{3, Symbol}} = nothing) where {S}
+function planartrace!(C::AbstractTensorMap{S,N₁,N₂}, pC::Index2Tuple{N₁,N₂},
+                      A::AbstractTensorMap{S}, pA::Index2Tuple{N₃,N₃},
+                      α, β) where {S,N₁,N₂,N₃}
+    if BraidingStyle(sectortype(S)) == Bosonic()
+        return tensortrace!(C, pC, A, pA, conjA, α, β)
+    end
+    
+    @boundscheck begin
+        all(i -> space(A, pC[1][i]) == space(C, i), 1:N₁) ||
+            throw(SpaceMismatch("trace: A = $(codomain(A))←$(domain(A)),
+                    C = $(codomain(C))←$(domain(C)), p1 = $(p1), p2 = $(p2)"))
+        all(i -> space(A, pC[2][i]) == space(C, N₁ + i), 1:N₂) ||
+            throw(SpaceMismatch("trace: A = $(codomain(A))←$(domain(A)),
+                    C = $(codomain(C))←$(domain(C)), p1 = $(p1), p2 = $(p2)"))
+        all(i -> space(A, pA[1][i]) == dual(space(A, pA[2][i])), 1:N₃) ||
+            throw(SpaceMismatch("trace: A = $(codomain(A))←$(domain(A)),
+                    q1 = $(q1), q2 = $(q2)"))
+    end
+    
+    if iszero(β)
+        fill!(C, β)
+    elseif β != 1
+        rmul!(C, β)
+    end
+    
+    pdata = linearize(pC)
+    for (f1, f2) in fusiontrees(A)
+        for ((f1′, f2′), coeff) in planar_trace(f1, f2, pC..., pA...)
+            TO._trace!(α * coeff, A[f1, f2], true, C[f1′, f2′], pdata, pA...)
+        end
+    end
+    return C
+end
 
+function planarcontract!(C::AbstractTensorMap{S,N₁,N₂}, pC::Index2Tuple{N₁,N₂},
+                      A::AbstractTensorMap{S}, pA::Index2Tuple, B::AbstractTensorMap{S}, pB::Index2Tuple, α, β) where {S,N₁,N₂}
     codA, domA = codomainind(A), domainind(A)
     codB, domB = codomainind(B), domainind(B)
-    oindA, cindA, oindB, cindB =
-        reorder_indices(codA, domA, codB, domB, oindA, cindA, oindB, cindB, p1, p2)
-
+    oindA, cindA, oindB, cindB = reorder_indices(codA, domA, codB, domB, pA..., pB[2], pB[1], pC...)
+    
     if oindA == codA && cindA == domA
         A′ = A
     else
-        if isnothing(syms)
-            A′ = TO.similar_from_indices(eltype(A), oindA, cindA, A, :N)
-        else
-            A′ = TO.cached_similar_from_indices(syms[1], eltype(A), oindA, cindA, A, :N)
-        end
+        A′ = TO.tensoralloc_add(scalartype(A), (oindA, cindA), A, :N)
         add_transpose!(true, A, false, A′, oindA, cindA)
     end
+    
     if cindB == codB && oindB == domB
         B′ = B
     else
-        if isnothing(syms)
-            B′ = TO.similar_from_indices(eltype(B), cindB, oindB, B, :N)
-        else
-            B′ = TO.cached_similar_from_indices(syms[2], eltype(B), cindB, oindB, B, :N)
-        end
+        B′ = TensorOperations.tensoralloc_add(scalartype(B), (cindB, oindB), B, :N)
         add_transpose!(true, B, false, B′, cindB, oindB)
     end
     mul!(C, A′, B′, α, β)
+    
     return C
 end
+
+# function planaradd!(α, tsrc::AbstractTensorMap{S},
+#                     β, tdst::AbstractTensorMap{S,N₁,N₂},
+#                     p1::IndexTuple{N₁}, p2::IndexTuple{N₂}) where {S,N₁,N₂}
+#     return add_transpose!(α, tsrc, β, tdst, p1, p2)
+# end
+
+# function planar_trace!(α, tsrc::AbstractTensorMap{S},
+#                        β, tdst::AbstractTensorMap{S,N₁,N₂},
+#                        p1::IndexTuple{N₁}, p2::IndexTuple{N₂},
+#                        q1::IndexTuple{N₃}, q2::IndexTuple{N₃}) where {S,N₁,N₂,N₃}
+#     if BraidingStyle(sectortype(S)) == Bosonic()
+#         return trace!(α, tsrc, β, tdst, p1, p2, q1, q2)
+#     end
+
+#     @boundscheck begin
+#         all(i -> space(tsrc, p1[i]) == space(tdst, i), 1:N₁) ||
+#             throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
+#                     tdst = $(codomain(tdst))←$(domain(tdst)), p1 = $(p1), p2 = $(p2)"))
+#         all(i -> space(tsrc, p2[i]) == space(tdst, N₁ + i), 1:N₂) ||
+#             throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
+#                     tdst = $(codomain(tdst))←$(domain(tdst)), p1 = $(p1), p2 = $(p2)"))
+#         all(i -> space(tsrc, q1[i]) == dual(space(tsrc, q2[i])), 1:N₃) ||
+#             throw(SpaceMismatch("trace: tsrc = $(codomain(tsrc))←$(domain(tsrc)),
+#                     q1 = $(q1), q2 = $(q2)"))
+#     end
+
+#     if iszero(β)
+#         fill!(tdst, β)
+#     elseif β != 1
+#         rmul!(tdst, β)
+#     end
+#     pdata = (p1..., p2...)
+#     for (f1, f2) in fusiontrees(tsrc)
+#         for ((f1′, f2′), coeff) in planar_trace(f1, f2, p1, p2, q1, q2)
+#             TO._trace!(α * coeff, tsrc[f1, f2], true, tdst[f1′, f2′], pdata, q1, q2)
+#         end
+#     end
+#     return tdst
+# end
+
+# function planar_contract!(α, A::AbstractTensorMap{S}, B::AbstractTensorMap{S},
+#                           β, C::AbstractTensorMap{S},
+#                           oindA::IndexTuple, cindA::IndexTuple,
+#                           oindB::IndexTuple, cindB::IndexTuple,
+#                           p1::IndexTuple, p2::IndexTuple,
+#                           syms::Union{Nothing,NTuple{3,Symbol}}=nothing) where {S}
+#     codA, domA = codomainind(A), domainind(A)
+#     codB, domB = codomainind(B), domainind(B)
+#     oindA, cindA, oindB, cindB = reorder_indices(codA, domA, codB, domB, oindA, cindA,
+#                                                  oindB, cindB, p1, p2)
+
+#     if oindA == codA && cindA == domA
+#         A′ = A
+#     else
+#         if isnothing(syms)
+#             A′ = TO.similar_from_indices(eltype(A), oindA, cindA, A, :N)
+#         else
+#             A′ = TO.cached_similar_from_indices(syms[1], eltype(A), oindA, cindA, A, :N)
+#         end
+#         add_transpose!(true, A, false, A′, oindA, cindA)
+#     end
+#     if cindB == codB && oindB == domB
+#         B′ = B
+#     else
+#         if isnothing(syms)
+#             B′ = TO.similar_from_indices(eltype(B), cindB, oindB, B, :N)
+#         else
+#             B′ = TO.cached_similar_from_indices(syms[2], eltype(B), cindB, oindB, B, :N)
+#         end
+#         add_transpose!(true, B, false, B′, cindB, oindB)
+#     end
+#     mul!(C, A′, B′, α, β)
+#     return C
+# end
 
 # auxiliary routines
 _cyclicpermute(t::Tuple) = (Base.tail(t)..., t[1])
@@ -95,10 +158,10 @@ function reorder_indices(codA, domA, codB, domB, oindA, oindB, p1, p2)
     while length(oindB2) > 0 && indB[end] != oindB2[1]
         indB = _cyclicpermute(indB)
     end
-    for i = 2:N₁
+    for i in 2:N₁
         @assert indA[i] == oindA2[i]
     end
-    for j = 2:N₂
+    for j in 2:N₂
         @assert indB[end + 1 - j] == oindB2[j]
     end
     Nc = length(indA) - N₁
@@ -110,12 +173,12 @@ function reorder_indices(codA, domA, codB, domB, oindA, oindB, p1, p2)
 end
 
 function reorder_indices(codA, domA, codB, domB, oindA, cindA, oindB, cindB, p1, p2)
-    oindA2, cindA2, oindB2, cindB2 =
-        reorder_indices(codA, domA, codB, domB, oindA, oindB, p1, p2)
+    oindA2, cindA2, oindB2, cindB2 = reorder_indices(codA, domA, codB, domB, oindA, oindB,
+                                                     p1, p2)
 
     #if oindA or oindB are empty, then reorder indices can only order it correctly up to a cyclic permutation!
     if isempty(oindA2) && !isempty(cindA)
-         # isempty(cindA) is a cornercase which I'm not sure if we can encounter
+        # isempty(cindA) is a cornercase which I'm not sure if we can encounter
         hit = cindA[findfirst(==(first(cindB2)), cindB)]
         while hit != first(cindA2)
             cindA2 = _cyclicpermute(cindA2)
