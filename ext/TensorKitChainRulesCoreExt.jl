484a323e6e79117f9d006f354f88206801c0b421
lkdvos <lukas.devos@ugent.be>
2023-10-16 23:21:33 +0200

    Less lazy `ProjectTo` fix

--- ext/TensorKitChainRulesCoreExt.jl
+++ ext/TensorKitChainRulesCoreExt.jl
@@ -59,7 +59,16 @@ function ChainRulesCore.rrule(::typeof(Base.copy), t::AbstractTensorMap)
 end
 
 ChainRulesCore.ProjectTo(::T) where {T<:AbstractTensorMap} = ProjectTo{T}()
-(::ProjectTo{T})(x::AbstractTensorMap) where {T<:AbstractTensorMap} = convert(T, x)
+function (::ProjectTo{T1})(x::T2) where {S,N1,N2,T1<:AbstractTensorMap{S,N1,N2},
+                                         T2<:AbstractTensorMap{S,N1,N2}}
+    T1 === T2 && return x
+    y = similar(x, scalartype(T1))
+    for (c, b) in blocks(y)
+        p = ProjectTo(b)
+        b .= p(block(x, c))
+    end
+    return y
+end
 
 # Base Linear Algebra
 # -------------------
