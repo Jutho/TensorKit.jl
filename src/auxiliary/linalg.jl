0027c484823d9c67feebcf99a0ebaac84e8c220e
Jutho <jutho.haegeman@ugent.be>
2024-01-27 00:19:17 +0100

    fix get_num_blas_threads

--- src/auxiliary/linalg.jl
+++ src/auxiliary/linalg.jl
@@ -1,7 +1,7 @@
 # Simple reference to getting and setting BLAS threads
 #------------------------------------------------------
 set_num_blas_threads(n::Integer) = LinearAlgebra.BLAS.set_num_threads(n)
-get_num_blas_threads(n::Integer) = LinearAlgebra.BLAS.get_num_threads(n)
+get_num_blas_threads() = LinearAlgebra.BLAS.get_num_threads()
 
 # Factorization algorithms
 #--------------------------
