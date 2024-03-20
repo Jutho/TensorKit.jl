e76eb51b1904de4bdaf259f899867ac9eedea09e
lkdvos <lukas.devos@ugent.be>
2024-03-20 09:07:54 +0100

    Formatter

--- TensorKitSectors/src/precompile.jl
+++ TensorKitSectors/src/precompile.jl
@@ -9,22 +9,22 @@ function precompile_sector(::Type{I}) where {I<:Sector}
     precompile(Rsymbol, (I, I, I))
     precompile(Asymbol, (I, I, I))
     precompile(Bsymbol, (I, I, I))
-    
+
     precompile(⊗, (I,))
     precompile(⊗, (I, I))
     precompile(⊗, (I, I, I))
     precompile(⊗, (I, I, I, I))
-    
+
     precompile(FusionStyle, (I,))
     precompile(BraidingStyle, (I,))
-    
+
     precompile(dim, (I,))
     precompile(sqrtdim, (I,))
     precompile(isqrtdim, (I,))
     precompile(dual, (I,))
     precompile(twist, (I,))
     precompile(frobeniusschur, (I,))
-    
+
     try
         precompile(fusiontensor, (I, I, I))
     catch
