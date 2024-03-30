078912f88d1804889e0acda81b3ddfebd408c664
Jutho <jutho.haegeman@ugent.be>
2024-03-30 22:35:48 +0100

    format

--- test/bugfixes.jl
+++ test/bugfixes.jl
@@ -1,6 +1,23 @@
 @timedtestset "Bugfixes" verbose = true begin
     @testset "BugfixConvert" begin
-        v = TensorMap(randn,ComplexF64,(Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((-3, 1/2, 1)=>3, (-5, 1/2, 1)=>10, (-7, 1/2, 1)=>13, (-9, 1/2, 1)=>9, (-11, 1/2, 1)=>1, (-5, 3/2, 1)=>3, (-7, 3/2, 1)=>3, (-9, 3/2, 1)=>1) ⊗ Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((1, 1/2, 1)=>1)') ← Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((-3, 1/2, 1)=>3, (-5, 1/2, 1)=>10, (-7, 1/2, 1)=>13, (-9, 1/2, 1)=>9, (-11, 1/2, 1)=>1, (-5, 3/2, 1)=>3, (-7, 3/2, 1)=>3, (-9, 3/2, 1)=>1))
+        v = TensorMap(randn, ComplexF64,
+                      (Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((-3, 1 / 2, 1) => 3,
+                                                                      (-5, 1 / 2, 1) => 10,
+                                                                      (-7, 1 / 2, 1) => 13,
+                                                                      (-9, 1 / 2, 1) => 9,
+                                                                      (-11, 1 / 2, 1) => 1,
+                                                                      (-5, 3 / 2, 1) => 3,
+                                                                      (-7, 3 / 2, 1) => 3,
+                                                                      (-9, 3 / 2, 1) => 1) ⊗
+                       Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((1, 1 / 2, 1) => 1)') ←
+                      Vect[(Irrep[U₁] ⊠ Irrep[SU₂] ⊠ FermionParity)]((-3, 1 / 2, 1) => 3,
+                                                                     (-5, 1 / 2, 1) => 10,
+                                                                     (-7, 1 / 2, 1) => 13,
+                                                                     (-9, 1 / 2, 1) => 9,
+                                                                     (-11, 1 / 2, 1) => 1,
+                                                                     (-5, 3 / 2, 1) => 3,
+                                                                     (-7, 3 / 2, 1) => 3,
+                                                                     (-9, 3 / 2, 1) => 1))
         w = convert(typeof(real(v)), v)
         @test w == v
         @test scalartype(w) == Float64
