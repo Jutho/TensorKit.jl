566f0599fc820e971d7509dd5888535247ded55e
Lukas Devos <ldevos98@gmail.com>
2024-11-03 20:08:49 -0500

    Add `AbstractTensorMap` as KrylovKit operator

--- ext/TensorKitKrylovKitExt.jl
+++ ext/TensorKitKrylovKitExt.jl
@@ -2,4 +2,9 @@ module TensorKitKrylovKitExt
 
 using TensorKit, KrylovKit
 
+# AbstractTensorMap as KrylovKit operator
+KrylovKit.apply(A::AbstractTensorMap, x::AbstractTensorMap) = A * x
+KrylovKit.apply_normal(A::AbstractTensorMap, x::AbstractTensorMap) = A * x
+KrylovKit.apply_adjoint(A::AbstractTensorMap, x::AbstractTensorMap) = A' * x
+
 end
