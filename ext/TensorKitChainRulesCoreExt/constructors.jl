1495391ef3f9978c25c98c22676d5e03158e5ef8
Jutho <Jutho@users.noreply.github.com>
2025-01-17 12:52:54 +0100

    fix permutedcopy_oftype rrule

--- ext/TensorKitChainRulesCoreExt/constructors.jl
+++ ext/TensorKitChainRulesCoreExt/constructors.jl
@@ -29,7 +29,7 @@ function ChainRulesCore.rrule(::typeof(TensorKit.permutedcopy_oftype), t::Abstra
     project = ProjectTo(t)
     function permutedcopy_oftype_pullback(Δt)
         invp = TensorKit._canonicalize(TupleTools.invperm(linearize(p)), t)
-        return project(TensorKit.permutedcopy_oftype(unthunk(Δt), scalartype(t), invp))
+        return project(TensorKit.permute(unthunk(Δt), invp))
     end
     return TensorKit.permutedcopy_oftype(t, T, p), permutedcopy_oftype_pullback
 end
