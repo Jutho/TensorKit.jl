a0e3460e7da2a069065f3f91cf736ed70c9b30ea
Yue Zhengyuan <yuezy1997@icloud.com>
2025-02-05 18:07:44 +0800

    Add DiagonalTensorMap constructors and converters (#212)
    * Update README upgrade guide; add TensorMap to DiagonalTensorMap conversion
    
    * Minor update to TensorMap to DiagonalTensorMap conversion
    
    * Add suggested changes
    
    * Refactoring
    
    * implement changes as discussed
    
    * restore tensor.jl
    
    * Restore README.md
    
    * Restore README.md 2
    
    * use `isdiag(::AbstractTensorMap)`
    
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>
    
    * Fix diagonal.jl
    
    * remove ambiguity in convert methods
    
    ---------
    
    Co-authored-by: Jutho <Jutho@users.noreply.github.com>
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- src/tensors/diagonal.jl
+++ src/tensors/diagonal.jl
@@ -52,6 +52,20 @@ function DiagonalTensorMap(data::DenseVector{T}, V::IndexSpace) where {T}
     return DiagonalTensorMap{T}(data, V)
 end
 
+function DiagonalTensorMap(t::AbstractTensorMap{T,S,1,1}) where {T,S}
+    isa(t, DiagonalTensorMap) && return t
+    domain(t) == codomain(t) ||
+        throw(SpaceMismatch("DiagonalTensorMap requires equal domain and codomain"))
+    A = storagetype(t)
+    d = DiagonalTensorMap{T,S,A}(undef, space(t, 1))
+    for (c, b) in blocks(d)
+        bt = block(t, c)
+        # TODO: rewrite in terms of `diagview` from MatrixAlgebraKit.jl
+        copy!(b.diag, view(bt, LinearAlgebra.diagind(bt)))
+    end
+    return d
+end
+
 # TODO: more constructors needed?
 
 # Special case adjoint:
@@ -73,12 +87,17 @@ end
 TensorMap(d::DiagonalTensorMap) = copy!(similar(d), d)
 Base.convert(::Type{TensorMap}, d::DiagonalTensorMap) = TensorMap(d)
 
-function Base.convert(::Type{DiagonalTensorMap{T,S,A}},
-                      d::DiagonalTensorMap{T,S,A}) where {T,S,A}
-    return d
-end
 function Base.convert(D::Type{<:DiagonalTensorMap}, d::DiagonalTensorMap)
-    return DiagonalTensorMap(convert(storagetype(D), d.data), d.domain)
+    return (d isa D) ? d : DiagonalTensorMap(convert(storagetype(D), d.data), d.domain)
+end
+Base.convert(::Type{DiagonalTensorMap}, t::DiagonalTensorMap) = t
+function Base.convert(::Type{DiagonalTensorMap}, t::AbstractTensorMap)
+    LinearAlgebra.isdiag(t) ||
+        throw(ArgumentError("DiagonalTensorMap requires input tensor that is diagonal"))
+    return DiagonalTensorMap(t)
+end
+function Base.convert(::Type{DiagonalTensorMap}, d::Dict{Symbol,Any})
+    return convert(DiagonalTensorMap, convert(TensorMap, d))
 end
 
 # Complex, real and imaginary parts
