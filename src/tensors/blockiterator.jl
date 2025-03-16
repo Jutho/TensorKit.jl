669f49e8653fd042e97b3091ac050e5b99f513e2
Lukas Devos <ldevos98@gmail.com>
2025-03-16 15:43:45 -0400

    implement `foreachblock`

--- src/tensors/blockiterator.jl
+++ src/tensors/blockiterator.jl
@@ -13,3 +13,28 @@ Base.IteratorEltype(::BlockIterator) = Base.HasEltype()
 Base.eltype(::Type{<:BlockIterator{T}}) where {T} = blocktype(T)
 Base.length(iter::BlockIterator) = length(iter.structure)
 Base.isdone(iter::BlockIterator, state...) = Base.isdone(iter.structure, state...)
+
+# TODO: fast-path when structures are the same?
+# TODO: do we want f(c, bs...) or f(c, bs)?
+# TODO: implement scheduler
+# TODO: do we prefer `blocks(t, ts...)` instead or as well?
+"""
+    foreachblock(f, t::AbstractTensorMap, ts::AbstractTensorMap...; [scheduler])
+
+Apply `f` to each block of `t` and the corresponding blocks of `ts`.
+Optionally, `scheduler` can be used to parallelize the computation.
+This function is equivalent to the following loop:
+
+```julia
+for (c, b) in blocks(t)
+    bs = (b, block.(ts, c)...)
+    f(c, bs)
+end
+```
+"""
+function foreachblock(f, t::AbstractTensorMap, ts::AbstractTensorMap...; scheduler=nothing)
+    foreach(blocks(t)) do (c, b)
+        return f(c, (b, block.(ts, c)...))
+    end
+    return nothing
+end
