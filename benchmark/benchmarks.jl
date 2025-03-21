6c9d1951847424c19c4514e88acbfa2e38903580
Lukas Devos <ldevos98@gmail.com>
2025-03-21 07:06:21 -0400

    Clean up

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,36 +1,17 @@
-params_path = get(ARGS, 1, joinpath("benchmark", "params.toml"))
+# Load benchmark code
+include("TensorKitBenchmarks/TensorKitBenchmarks.jl")
+const SUITE = TensorKitBenchmarks.SUITE
 
-using BenchmarkTools
-using TensorKit
-using TOML
-
-include("utils.jl")
-# using .BenchmarkUtils
-
-include("bench_linalg.jl")
-include("bench_indexmanipulations.jl")
-include("bench_tensornetworks.jl")
-
-SUITE = BenchmarkGroup()
-
-# add some child groups
-SUITE["linalg"] = BenchmarkGroup()
-SUITE["indexmanipulations"] = BenchmarkGroup()
-SUITE["tensornetworks"] = BenchmarkGroup()
-
-# --------------------------------------------------------------------------------------- #
-# load parameters
-# --------------------------------------------------------------------------------------- #
-
-all_parameters = TOML.parsefile(params_path)
-
-for (group_id, group) in all_parameters
-    haskey(SUITE, group_id) || addgroup!(SUITE, group_id)
-
-    for (bench_id, params) in group
-        @info "$group_id : $bench_id"
-        @assert params isa Vector
-        f = eval(Meta.parse("benchmark_" * bench_id * "!"))
-        f.(Ref(SUITE[group_id]), params)
+# Populate benchmarks
+# Detect if user supplied extra arguments to load only specific modules
+# e.g. julia benchmarks.jl --modules=linalg,tensornetworks
+modules_pattern = r"(?:--modules=)(\w+)"
+arg_id = findfirst(contains(modules_pattern), ARGS)
+if isnothing(arg_id)
+    TensorKitBenchmarks.loadall!()
+else
+    modules = split(only(match(modules_pattern, ARGS[arg_id]).captures[1]), ",")
+    for m in modules
+        TensorKitBenchmarks.load!(m)
     end
 end
