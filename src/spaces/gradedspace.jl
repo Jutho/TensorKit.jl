4bc1e7c38b4b84661f409789e2ba62b3a28efe46
Boris De Vos <boris.devos@ugent.be>
2025-04-14 17:11:46 +0200

    Revert "change dim of GradedSpace to not evaluate one"
    This reverts commit c31b99c7a0cf80aa4e91afe97987f9fcabe82a67.

--- src/spaces/gradedspace.jl
+++ src/spaces/gradedspace.jl
@@ -90,9 +90,9 @@ Base.hash(V::GradedSpace, h::UInt) = hash(V.dual, hash(V.dims, h))
 # properties
 field(::Type{<:GradedSpace}) = ℂ
 InnerProductStyle(::Type{<:GradedSpace}) = EuclideanInnerProduct()
-function dim(V::GradedSpace{I}) where {I<:Sector}
+function dim(V::GradedSpace)
     return reduce(+, dim(V, c) * dim(c) for c in sectors(V);
-                  init=zero(TensorKitSectors._Fscalartype(I)))
+                  init=zero(dim(one(sectortype(V)))))
 end
 function dim(V::GradedSpace{I,<:AbstractDict}, c::I) where {I<:Sector}
     return get(V.dims, isdual(V) ? dual(c) : c, 0)
