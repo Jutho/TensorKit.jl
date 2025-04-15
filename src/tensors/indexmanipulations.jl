428caf01b34e30c7384a4c16ea38ae38d374a6a9
Sander De Meyer <74001142+sanderdemeyer@users.noreply.github.com>
2025-04-15 14:18:32 +0200

    Update src/tensors/indexmanipulations.jl
    fix spelling mistake
    
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- src/tensors/indexmanipulations.jl
+++ src/tensors/indexmanipulations.jl
@@ -9,7 +9,7 @@ Return a new tensor that is isomorphic to `t` but where the arrows on the indice
 !!! note
     The isomorphism that `flip` applies to each of the indices `i ∈ I` is such that flipping two indices
     that are afterwards contracted within an `@tensor` contraction will yield the same result as without
-    flipping those indices first. However, `flip` is not involutary, i.e. `flip(flip(t, I), I) != t` in
+    flipping those indices first. However, `flip` is not involutory, i.e. `flip(flip(t, I), I) != t` in
     general. To obtain the original tensor, one can use the `inv` keyword, i.e. it holds that
     `flip(flip(t, I), I; inv=true) == t`.
 """
