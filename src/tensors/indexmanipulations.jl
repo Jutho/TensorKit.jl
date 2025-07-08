76b780514a541368312cca758a7c1e502de26de2
lkdvos <ldevos98@gmail.com>
2025-07-08 12:41:31 -0400

    Hand-crafted `copyto!` specialization

--- src/tensors/indexmanipulations.jl
+++ src/tensors/indexmanipulations.jl
@@ -650,7 +650,7 @@ function _add_transform_multi!(tdst, tsrc, p,
     buffer_src = StridedView(buffer2, (blocksize, cols), (1, blocksize), 0)
     for (i, struct_src) in enumerate(structs_src)
         subblock_src = sreshape(StridedView(tsrc.data, sz_src, struct_src...), matsize)
-        copyto!(buffer_src[:, i], subblock_src)
+        _copyto!(buffer_src[:, i], subblock_src)
     end
 
     # Resummation into a second buffer using BLAS
