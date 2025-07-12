150f4d6a1920378b365fea565681ee19a4a73d53
Lukas Devos <ldevos98@gmail.com>
2025-07-12 08:58:36 -0400

    Correctly handle dual of sectors

--- src/tensors/treetransformers.jl
+++ src/tensors/treetransformers.jl
@@ -65,12 +65,12 @@ function GenericTreeTransformer(transform, p, Vdst, Vsrc)
     I = sectortype(Vsrc)
 
     uncoupleds_src = map(structure_src.fusiontreelist) do (f₁, f₂)
-        return TupleTools.vcat(f₁.uncoupled, f₂.uncoupled)
+        return TupleTools.vcat(f₁.uncoupled, dual.(f₂.uncoupled))
     end
     uncoupleds_src_unique = unique(uncoupleds_src)
 
     uncoupleds_dst = map(structure_dst.fusiontreelist) do (f₁, f₂)
-        return TupleTools.vcat(f₁.uncoupled, f₂.uncoupled)
+        return TupleTools.vcat(f₁.uncoupled, dual.(f₂.uncoupled))
     end
 
     T = sectorscalartype(I)
