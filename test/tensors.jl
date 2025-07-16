301780aacbb58c1d460c27d200fa59a68b555e99
Lukas Devos <ldevos98@gmail.com>
2025-07-16 17:17:14 -0400

    Adapt tests to `eltype(blocks)`

--- test/tensors.jl
+++ test/tensors.jl
@@ -47,7 +47,8 @@ for V in spacelist
                 next = @constinferred Nothing iterate(bs, state)
                 b2 = @constinferred block(t, first(blocksectors(t)))
                 @test b1 == b2
-                @test eltype(bs) === typeof(b1) === TensorKit.blocktype(t)
+                @test eltype(bs) === Pair{typeof(c),typeof(b1)}
+                @test typeof(b1) === TensorKit.blocktype(t)
             end
         end
         @timedtestset "Tensor Dict conversion" begin
