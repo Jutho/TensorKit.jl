d9152738e5087439e605e7b6e5c964865ad838eb
Lukas Devos <ldevos98@gmail.com>
2025-07-16 17:15:44 -0400

    Remove `isdone`

--- src/tensors/blockiterator.jl
+++ src/tensors/blockiterator.jl
@@ -12,4 +12,3 @@ Base.IteratorSize(::BlockIterator) = Base.HasLength()
 Base.IteratorEltype(::BlockIterator) = Base.HasEltype()
 Base.eltype(::Type{<:BlockIterator{T}}) where {T} = Pair{sectortype(T),blocktype(T)}
 Base.length(iter::BlockIterator) = length(iter.structure)
-Base.isdone(iter::BlockIterator, state...) = Base.isdone(iter.structure, state...)
