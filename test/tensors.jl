9d543ee1435d7bd5335c8c417b871acda6ee322a
Boris De Vos <boris.devos@ugent.be>
2025-07-17 10:20:44 +0200

    format

--- test/tensors.jl
+++ test/tensors.jl
@@ -109,7 +109,7 @@ for V in spacelist
                 next = @constinferred Nothing iterate(bs, state)
                 b2 = @constinferred block(t', first(blocksectors(t')))
                 @test b1 == b2
-                @test eltype(bs) === Pair{typeof(c), typeof(b1)}
+                @test eltype(bs) === Pair{typeof(c),typeof(b1)}
                 @test typeof(b1) === TensorKit.blocktype(t')
                 @test typeof(c) === sectortype(t)
                 # linear algebra
