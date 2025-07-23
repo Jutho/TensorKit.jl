19ba22509cd1a789134cb655d443ccc201569720
Boris De Vos <boris.devos@ugent.be>
2025-07-23 14:31:01 -0400

    finish up multifusion space tests

--- test/multifusion.jl
+++ test/multifusion.jl
@@ -38,12 +38,15 @@ ti = time()
     Wright = @constinferred Vect[I](I(2, 2, 0) => 1, I(2, 2, 1) => 1)
     WM = @constinferred Vect[I](I(1, 2, 0) => 1)
     WMop = @constinferred Vect[I](I(2, 1, 0) => 1)
-    @test W == GradedSpace(one(I) => 1, randsector(I) => 0)
-    @test @constinferred(oneunit(V)) == W == oneunit(typeof(V))
 
-    # randsector never returns trivial sector, so this cannot error
-    @test_throws ArgumentError GradedSpace(one(I) => 1, randsector(I) => 0, one(I) => 3)
-    @test eval(Meta.parse(sprint(show, W))) == W
+    @test @constinferred(oneunit(Wleft)) == leftoneunit(Wleft) == rightoneunit(Wleft)
+    @test @constinferred(oneunit(Wright)) == leftoneunit(Wright) == rightoneunit(Wright)
+    @test @constinferred(leftoneunit(⊕(Wleft, WM))) == oneunit(Wleft)
+    @test @constinferred(rightoneunit(⊕(Wright, WMop))) == oneunit(Wright)
+
+    @test_throws ArgumentError oneunit(I)
+    @test_throws ArgumentError oneunit(WM)
+    @test_throws ArgumentError oneunit(WMop)
 
     @test isa(V, VectorSpace)
     @test isa(V, ElementarySpace)
@@ -52,7 +55,7 @@ ti = time()
     @test isa(V, GradedSpace)
     @test isa(V, GradedSpace{I})
     @test @constinferred(dual(V)) == @constinferred(conj(V)) ==
-            @constinferred(adjoint(V)) != V
+          @constinferred(adjoint(V)) != V
     @test @constinferred(field(V)) == ℂ
     @test @constinferred(sectortype(V)) == I
     slist = @constinferred sectors(V)
@@ -64,9 +67,31 @@ ti = time()
     @test @constinferred(⊕(V, zero(V))) == V
     @test @constinferred(⊕(V, V)) == Vect[I](c => 2dim(V, c) for c in sectors(V))
     @test @constinferred(⊕(V, V, V, V)) == Vect[I](c => 4dim(V, c) for c in sectors(V))
-    # @test @constinferred(⊕(V, oneunit(V))) ==
-    #         Vect[I](c => isone(c) + dim(V, c) for c in sectors(V))
-    # @test @constinferred(fuse(V, oneunit(V))) == V
+
+    for W in [Wleft, Wright]
+        @test @constinferred(⊕(W, oneunit(W))) ==
+              Vect[I](c => isone(c) + dim(W, c) for c in sectors(W))
+        @test @constinferred(fuse(W, oneunit(W))) == W
+    end
+
+    # sensible direct sums and fuses
+    @test @constinferred(⊕(Wleft, WM)) ==
+          Vect[I](c => 1 for c in sectors(V) if leftone(c) == I(1, 1, 0))
+    @test @constinferred(⊕(Wright, WMop)) ==
+          Vect[I](c => 1 for c in sectors(V) if leftone(c) == I(2, 2, 0))
+    @test @constinferred(⊕(Wright, WM)) ==
+          Vect[I](c => 1 for c in sectors(V) if rightone(c) == I(2, 2, 0))
+    @test @constinferred(⊕(Wleft, WMop)) ==
+          Vect[I](c => 1 for c in sectors(V) if rightone(c) == I(1, 1, 0))
+    @test @constinferred(fuse(Wleft, WM)) == Vect[I](I(1, 2, 0) => 2)
+    @test @constinferred(fuse(Wright, WMop)) == Vect[I](I(2, 1, 0) => 2)
+
+    # less sensible direct sums and fuses
+    @test @constinferred(⊕(Wleft, Wright)) ==
+          Vect[I](c => 1 for c in sectors(V) if leftone(c) == rightone(c))
+    @test @constinferred(fuse(Wleft, WMop)) == fuse(Wright, WM) ==
+          Vect[I](c => 0 for c in sectors(V))
+
     d = Dict{I,Int}()
     for a in sectors(V), b in sectors(V)
         for c in a ⊗ b
@@ -75,7 +100,7 @@ ti = time()
     end
     @test @constinferred(fuse(V, V)) == GradedSpace(d)
     @test @constinferred(flip(V)) ==
-            Vect[I](conj(c) => dim(V, c) for c in sectors(V))'
+          Vect[I](conj(c) => dim(V, c) for c in sectors(V))'
     @test flip(V) ≅ V
     @test flip(V) ≾ V
     @test flip(V) ≿ V
@@ -83,10 +108,15 @@ ti = time()
     @test V == @constinferred infimum(V, ⊕(V, V))
     @test V ≺ ⊕(V, V)
     @test !(V ≻ ⊕(V, V))
-    # @test infimum(V, GradedSpace(one(I) => 3)) == GradedSpace(one(I) => 2)
+    @test infimum(V, GradedSpace(I(1, 1, 0) => 3)) ==
+          GradedSpace(I(1, 1, 0) => 2)
+    @test infimum(V, GradedSpace(I(1, 2, 0) => 6)) ==
+          GradedSpace(I(1, 2, 0) => 5)
+    for W in [WM, WMop, Wright]
+        @test infimum(Wleft, W) == Vect[I](c => 0 for c in sectors(V))
+    end
     @test_throws SpaceMismatch (⊕(V, V'))
 end
-
 
 tf = time()
 printstyled("Finished multifusion tests in ",
