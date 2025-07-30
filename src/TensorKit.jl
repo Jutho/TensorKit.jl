490aa4fc39a29f33db3e087debd0a2fd384c20c1
Boris De Vos <boris.devos@ugent.be>
2025-07-30 16:15:40 +0200

    export `left/rightoneunit`

--- src/TensorKit.jl
+++ src/TensorKit.jl
@@ -32,7 +32,7 @@ export SpaceMismatch, SectorMismatch, IndexError # error types
 
 # general vector space methods
 export space, field, dual, dim, reduceddim, dims, fuse, flip, isdual, oplus,
-       insertleftunit, insertrightunit, removeunit
+       leftoneunit, rightoneunit, insertleftunit, insertrightunit, removeunit
 
 # partial order for vector spaces
 export infimum, supremum, isisomorphic, ismonomorphic, isepimorphic
